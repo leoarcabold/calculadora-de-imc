19b988fed5719994eef093c9989b57e38226b432
Leandro Soares <leoarcabold@gmail.com>
2022-08-06 08:09:23 -0300

    refactor: organizando o código

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-function start (){
-
-    var buttonCalculateImc = document.querySelector('#button-calculate-imc')
-
-    console.log(buttonCalculateImc);
+function start () {
+    var buttonCalcularImc = document.querySelector('#button-calcular-imc');
+    buttonCalcularImc.addEventListener('click', handleButtonClick());
 }
 
-function calculateImc(weight, heith){
-    return (weight / (heith*heith));
+function calculateImc(peso, altura){
+    return (peso / (altura*altura));
+}
+
+function handleButtonClick() {
+    var inputPeso = document.querySelector('#input-peso');
+    var inputAltura = document.querySelector('#input-altura');
+console.log(inputPeso);
+console.log(inputAltura);
 }
 
  start();
