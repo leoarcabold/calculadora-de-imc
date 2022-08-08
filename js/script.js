8102ad92c41cc253e16bfa42a9bcce9c6f60fe70
Leandro Soares <leoarcabold@gmail.com>
2022-08-07 21:13:05 -0300

    feat: add function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 function start () {
     var buttonCalcularImc = document.querySelector('#button-calcular-imc');
-    buttonCalcularImc.addEventListener('click', handleButtonClick());
+    buttonCalcularImc.addEventListener('click', handleButtonClick);
+
+    var inputPeso = document.querySelector('#input-peso');
+    var inputAltura = document.querySelector('#input-altura');
+
+    inputPeso.addEventListener('input', handleButtonClick);
+    inputAltura.addEventListener('input', handleButtonClick);
+
+
+    handleButtonClick();
+    
+
 }
 
 function calcularImc(peso, altura){
@@ -14,10 +25,45 @@ function handleButtonClick() {
 
     var peso = Number(inputPeso.value);
     var altura = Number(inputAltura.value);
-    var imcFormatado = toFixed(2);
+
+   
 var imc = calcularImc(peso,altura);
-imcResultado.textContent = imc;
+
+var imcFormatado = imc.toFixed(2).replace('.',',');
+
+imcResultado.textContent = imcFormatado;
+paragrafo-tipo-imc.textContent = 
+
+classificaTipoImc();
 
 }
+
+function classificaTipoImc (imc){
+
+    var validaImc = imc;
+if(imc >= 16 && imc <= 16.9){
+    console.log('Muito abaixo do peso');
+}
+if(imc >= 17 && imc <= 18.4){
+    console.log('Abaixo do peso');
+}
+if(imc >= 18.5 && imc <= 24.9){
+    console.log('Peso normal');
+}
+if(imc >= 25 && imc <= 29.9){
+    console.log('Acima do peso');
+}
+if(imc >= 30 && imc <= 34.9){
+    console.log('Obesidade grau I');
+}
+if(imc >= 35 && imc <= 40){
+    console.log('Obesidade grau II');
+}
+if(imc > 40){
+    console.log('Obesidade grau III');
+}
+
+}
+
 
  start();
